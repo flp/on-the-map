6cb864243710a7021ffa3b4f6d0f44546613aac4
Franklin Pearsall <franklin.pearsall@gmail.com>
2016-05-01 19:36:23 -0700

    Change parse query to fetch to 100 most recently updated entries.

--- On the Map/ParseClient.swift
+++ On the Map/ParseClient.swift
@@ -19,7 +19,7 @@ class ParseClient: NSObject {
             completionHandler(fetchedStudents: nil, error: NSError(domain: "login", code: 1, userInfo: userInfo))
         }
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?limit=100&order=-updatedAt")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         
